0cc8234bad617452b41eba5d77c02a6ab23b7c04
keithMaybe <lzt199094@163.com>
2017-03-25 21:34:30 +0800

    Mat

---
+++ opencv/opencv学习(一)之Mat创建/CMakeFiles/mat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mat.dir/mat.cpp.o"
+  "mat.pdb"
+  "mat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
